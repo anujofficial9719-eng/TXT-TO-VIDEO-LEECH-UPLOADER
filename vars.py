78574105fceef23a9166ecff0aeda6c3224695c8
anujofficial9719-eng <anujofficial9719@gmail.com>
2026-03-16 14:02:53 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -2,22 +2,22 @@
 
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+API_ID = int(environ.get("API_ID", "34724970"))
+API_HASH = environ.get("API_HASH", "f240eae7c60e8e30c17203ab0e052f7e")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8656231605:AAGVZvUNt7VJiqq1r7MT_wbhCLO229ztU2I")
 
 # Force Subscribe Configuration
 FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "bot_subscription")  # Channel username without @, 
-FORCE_SUB_CHANNEL_LINK = environ.get("FORCE_SUB_CHANNEL_LINK", "https://t.me/bot_subscription")  # Channel link
+FORCE_SUB_CHANNEL_LINK = environ.get("FORCE_SUB_CHANNEL_LINK", "https://t.me/anujedits76")  # Channel link
 
 # Admin Configuration
-ADMINS = list(map(int, environ.get("ADMINS", "").split()))
+ADMINS = list(map(int, environ.get("ADMINS", "7521421400").split()))
 
 # Optional: Bot Owner ID
-OWNER_ID = int(environ.get("OWNER_ID", ""))
+OWNER_ID = int(environ.get("OWNER_ID", "7521421400"))
 
 # Database URL (if you want to add database support later)
-DATABASE_URL = environ.get("DATABASE_URL", "")
+DATABASE_URL = environ.get("DATABASE_URL", "mongodb+srv://Anujedit:Anujedit@cluster0.7cs2nhd.mongodb.net/?appName=Cluster0")
 
 
 
